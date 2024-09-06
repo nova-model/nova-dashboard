2c93b711b857c28d821e292ca6d0b183ec002c1e
Gregory Cage <cagege@ornl.gov>
2024-09-06 15:34:32 -0400

    Adjust run script settings

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-(cd vue; pnpm run dev --host 0.0.0.0 --port 5173) & python django/manage.py migrate && python django/manage.py runserver_plus --insecure 0.0.0.0:8080
+(cd vue; pnpm run dev --host 0.0.0.0 --port 5173) & python ./manage.py migrate && python ./manage.py runserver_plus --insecure 0.0.0.0:8080
